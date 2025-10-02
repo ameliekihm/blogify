92ef8f36a5798e3c01c2e70393d1d7ed88543c20
ameliekihm <ameliekihm@gmail.com>
2025-10-01 23:19:21 -0600

    style: improve login button and user info layout

--- src/components/auth-header.ts
+++ src/components/auth-header.ts
@@ -14,13 +14,15 @@ export async function initAuthHeader() {
 
   if (user) {
     header.innerHTML = `
-      <img src="${user.photo}" width="24" height="24" style="border-radius:50%; margin-right:8px;">
-      <span>${user.firstName} ${user.lastName}</span>
       <button id="logout-btn">Logout</button>
+      <div class="user-info">
+        <img src="${user.photo}" width="32" height="32" style="border-radius:50%;">
+        <span>${user.firstName}</span>
+      </div>
     `;
     document.querySelector('#logout-btn')?.addEventListener('click', logout);
   } else {
-    header.innerHTML = `<button id="login-btn">Login with Google</button>`;
+    header.innerHTML = `<button id="login-btn">Login</button>`;
     document
       .querySelector('#login-btn')
       ?.addEventListener('click', loginWithGoogle);
